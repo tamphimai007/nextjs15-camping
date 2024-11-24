78c7e657880b3864af132fc354c19e16d98abcde
tamisan <phongphat.jap@gmail.com>
2024-11-24 20:52:43 +0700

    EP17 TextArea&Province&Price

--- app/camp/create/page.tsx
+++ app/camp/create/page.tsx
@@ -3,35 +3,44 @@ import { SubmitButton } from "@/components/form/Buttons";
 import CategoryInput from "@/components/form/CategoryInput";
 import FormContainer from "@/components/form/FormContainer";
 import FormInput from "@/components/form/FormInput";
+import ProvinceInput from "@/components/form/ProvinceInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
-const CreateProfile = async() => {
-
-
+const CreateProfile = async () => {
   return (
     <section>
-      <h1 className="text-2xl font-semibold mb-8 capitalize">Create Landmark</h1>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">
+        Create Landmark
+      </h1>
       <div className="border p-8 rounded-md">
-
         <FormContainer action={createLandmarkAction}>
           <div className="grid md:grid-cols-2 gap-4 mt-4">
-          <FormInput 
-            name="name" 
-            label="Landmark Name" 
-            type="text" 
-            placeholder="Landmark Name" />
+            <FormInput
+              name="name"
+              label="Landmark Name"
+              type="text"
+              placeholder="Landmark Name"
+            />
 
-          {/* Category */}
-          <CategoryInput />
-          
+            {/* Category */}
+            <CategoryInput />
+          </div>
+          <TextAreaInput name="description" />
+          <div className="grid md:grid-cols-2 gap-4 mt-4">
+            <FormInput
+              name="price"
+              label="Price"
+              type="number"
+              placeholder="Price"
+            />
+            
+            <ProvinceInput />
           </div>
 
-          <SubmitButton text="create Landmark" size='lg' />
+          <SubmitButton text="create Landmark" size="lg" />
         </FormContainer>
-        
-
       </div>
     </section>
   );
